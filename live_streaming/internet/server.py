2006763bf282046b86c31882bc669adbc5e6032a
Maeglin Liao <maeglin89273@gmail.com>
2015-07-06 19:26:44 +0800

    #Fix the issue when connection close from the client

--- live_streaming/internet/server.py
+++ live_streaming/internet/server.py
@@ -16,19 +16,31 @@ def direct_to_model(raw_data):
     print raw_data
 
 
+def is_end(json_data):
+    #empty string OR eot, end of transimisson OR spaces
+    return not json_data or \
+    (len(json_data) == 1 and ord(json_data) == 4) or \
+    json_data.isspace()
+
+
 def worker_thread(conn, client_addr):
-    print "accept a pair of slippers from " + client_addr[0] + ":" + str(client_addr[1])
+    slipper_addr_str = client_addr[0] + ":" + str(client_addr[1])
+    print "accept a pair of slippers from " + slipper_addr_str
     try:
         while True:
             json_data = conn.recv(BUFFER_SIZE)
-            
-            if json_data.isspace() or not json_data:
+
+            if is_end(json_data):
                 break
 
             raw_data = json.loads(json_data)
             direct_to_model(raw_data)
+    except:
+        pass
+
     finally:
         conn.close()
+        print "disconnect the slippers from " + slipper_addr_str
 
 def start_server():
     print 'current ip address: ' + sk.gethostbyname(sk.gethostname())
