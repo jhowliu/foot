e1e7ebec449e4f4e8fc0b6e40acf75df6a8e1ff8
TomLong <boy82530ten@gmail.com>
2015-09-01 15:06:59 +0800

    debug

---
+++ live_streaming/internet/live_data/udp_server.py
@@ -0,0 +1,37 @@
+import SocketServer
+import json
+
+__author__ = 'maeglin89273'
+
+import socket as sk
+
+HOST = ''
+PORT = 3070
+
+BUFFER_SIZE = 512
+
+filename = 'Terry.csv'
+
+out = open(filename, 'a')
+
+def direct_to_model(raw_data):
+    data = [raw_data['FFA2'], raw_data['FFA3'], raw_data['FFA4'], raw_data['FFA6'], raw_data['FFA7'], raw_data['FFA8'], raw_data['Timestamp'], raw_data['Label']]
+
+    print data[:3]
+    out.write(','.join([str(x) for x in data]) + '\n')
+
+class UDPHandler(SocketServer.BaseRequestHandler):
+    def handle(self):
+        json_data = self.request[0]
+        raw_data = json.loads(json_data)
+        direct_to_model(raw_data)
+
+def start_server():
+    print 'current ip address: ' + sk.gethostbyname(sk.gethostname())
+
+    server = SocketServer.UDPServer((HOST, PORT), UDPHandler)
+    server.serve_forever()
+
+
+if __name__ == '__main__':
+    start_server()
